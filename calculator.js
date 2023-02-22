f7be73cc9c3b79a10d7f2557956aaf59d31392bb
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2023-02-22 15:52:54 +0100

    Update

--- calculator.js
+++ calculator.js
@@ -1,22 +1,31 @@
 window.addEventListener("DOMContentLoaded", ()=>{
-    const routeDatas = loadRouteData("");
-    console.log(routeDatas)
+    loadRouteData(""); // get everyting
 });
-
-function fillTableWithDatas(datas){
-
-}
 
 function loadRouteData(stageID){
     getRouteData("https://ub2023-backend.onrender.com/api/v1/stages/"+stageID).then(response=>{
         console.log(response)
+        fillTableWithDatas(response);
     }).catch(err=>{
         console.log(err)
     })
 }
 
+function fillTableWithDatas(dataList){
+    const table = document.getElementById("stageAssignmentTable");
+
+    for(let datas of dataList){
+        let row = table.insertRow();
+        row.classList.add("stageAssignmentTr");
+        for(let data in datas){
+            let cell = row.insertCell();
+            cell.innerHTML = `${datas[data]}`;
+        }
+    }
+}
+
 function getRouteData(url){
-    new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject)=>{
         fetch(url).then(res=>{
             res.json().then(json=>{
                 resolve(json)
