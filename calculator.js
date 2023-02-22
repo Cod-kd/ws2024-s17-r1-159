71e04a684a2aa881552af9bada3b9a0b64c7a7b3
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2023-02-22 22:11:46 +0100

    Update

--- calculator.js
+++ calculator.js
@@ -1,27 +1,62 @@
 window.addEventListener("DOMContentLoaded", ()=>{
-    loadRouteData(""); // get everyting
+    fillTeamMemberTableWithDatas("")
+    loadStageData(""); // get everyting from the API
+
 });
 
-function loadRouteData(stageID){
+function fillTeamMemberTableWithDatas(dataList){
+    const table = document.getElementById("teamMemeberTable");
+
+    // fill the table with data
+    for(let c = 1; c <= 10; c++){
+        let row = table.insertRow();
+        row.innerHTML = `
+                        <td>${c}</td>
+                        <td><input type="text" placeholder="First name.."></td>
+                        <td><input type="text" placeholder="Last name.."></td>
+                        <td><input type="text" placeholder="MM:SS / km"></td>
+                        <td>${0} km</td>
+                        `;
+        row.classList.add("teamMemeberTr");
+                        
+    }
+}
+function fillStageTableWithDatas(dataList){
+    const table = document.getElementById("stageAssignmentTable");
+
+    // fill the table with data
+    for(let datas of dataList){
+        let row = table.insertRow();
+        row.innerHTML = `
+                        <td>${datas.id}</td>
+                        <td>${datas.distance} km</td>
+                        <td>${datas.startingLocation}</td>
+                        <td>${datas.arrivalLocation}</td>
+                        <td>${datas.name == ""? "---" : datas.name}</td>
+                        <td>${chooseLocalRunner(["j", "k", "l"])}</td>
+                        <td>MM:SS</td>
+                        `;
+        row.classList.add("stageAssignmentTr");
+                        
+    }
+}
+
+function loadStageData(stageID){
     getRouteData("https://ub2023-backend.onrender.com/api/v1/stages/"+stageID).then(response=>{
         console.log(response)
-        fillTableWithDatas(response);
+        fillStageTableWithDatas(response);
     }).catch(err=>{
         console.log(err)
     })
 }
 
-function fillTableWithDatas(dataList){
-    const table = document.getElementById("stageAssignmentTable");
-
-    for(let datas of dataList){
-        let row = table.insertRow();
-        row.classList.add("stageAssignmentTr");
-        for(let data in datas){
-            let cell = row.insertCell();
-            cell.innerHTML = `${datas[data]}`;
-        }
+function chooseLocalRunner(localRunners){
+    dataListHTML = `<input list="runners" placeholder="No runner...">
+                    <datalist id="runners">`;
+    for(let runner of localRunners){
+        dataListHTML += `<option value="${runner}">`;
     }
+    return dataListHTML + `</datalist>`;
 }
 
 function getRouteData(url){
